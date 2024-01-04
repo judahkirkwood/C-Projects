b2f8867272112eec6369445395e09b57d650fb5a
judahkirkwood <judahkirkwood@gmail.com>
2024-01-04 11:28:10 -0800

    C#
    Test Site

---
+++ WebAppTutorial/WebAppTutorial/wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+function newColor() {
+    document.getElementById("test_page").style.color = "blue";
+}
+
+function oldColor() {
+    document.getElementById("test_page").style.color = "lightgreen";
+}
+// Write your JavaScript code.
