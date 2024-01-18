5ae14bec6445de6ce2c29c1d8b7a7c2bbfd8985e
judahkirkwood <judahkirkwood@gmail.com>
2024-01-18 13:28:45 -0800

    Conditional Statement written

---
+++ ConditionalStatements/ConditionalStatements/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Gibson";
+
+            if (name == "Fender")
+            {
+                Console.WriteLine("This is a Fender guitar.");
+            }
+            else if (name == "Gibson")
+            {
+                Console.WriteLine("This is a Gibson guitar.");
+            }
+            else if (name != "Gibson" && name != "Fender")
+            {
+                Console.WriteLine("This guitar is not in our database. Please try again.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
