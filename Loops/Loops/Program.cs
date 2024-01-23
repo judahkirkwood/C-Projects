eb67846cdc7984584a283605264141657ebde9eb
judahkirkwood <judahkirkwood@gmail.com>
2024-01-23 02:10:16 -0800

    Loops update

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Which Guardian class is best?");
+            string guardian = Convert.ToString(Console.ReadLine());
+            bool isGuessed = guardian == "Hunter";
+
+            do
+            {
+                switch (guardian)
+                {
+                    case "Titan":
+                        Console.WriteLine("You are literally the worst. May the Traveler have mercy on your poor soul. Stop eating crayons and try again.");
+                        Console.WriteLine("Which class is best?");
+                        guardian = Convert.ToString(Console.ReadLine());
+                        break;
+                    case "Warlock":
+                        Console.WriteLine("You guessed Warlock... they are great, but drop that rift and try again.");
+                        Console.WriteLine("Which class is best?");
+                        guardian = Convert.ToString(Console.ReadLine());
+                        break;
+                    case "Hunter":
+                        Console.WriteLine("You guessed Hunter. That is correct! Hunters are THE BEST! You are the Traveler's Chosen!Hunter");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong.");
+                        Console.WriteLine("Guess again?");
+                        guardian = Convert.ToString(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!isGuessed);
+
+            Console.Read();
+        }
+    }
+}
+
