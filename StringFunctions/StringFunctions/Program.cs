ee89183005943442f15af81c2d9039e5216ab8fc
judahkirkwood <judahkirkwood@gmail.com>
2024-01-23 16:31:30 -0800

    StringFunctions added

---
+++ StringFunctions/StringFunctions/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Judah ";
+            string greeting = "Hello, ";
+            string sentence = "it is a good day to code.";
+            string combineStrings = greeting + name + sentence;
+            string sentence1 = "This is my first sentence.\n";
+            string sentence2 = "This is my second sentence.\n";
+            string sentence3 = "This is my third sentence.\n";
+
+
+            //bool trueOrFalse = name.Contains("s");
+            //trueOrFalse = name.EndsWith("s");
+
+            //int length = name.Length;
+            //converts sting to UPPER CASE
+            combineStrings = combineStrings.ToUpper();
+
+            Console.WriteLine(combineStrings);
+            
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append (sentence1 + sentence2 + sentence3);
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
