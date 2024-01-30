f1a7128c8940586f56a5d4b1dc5163350898c9d8
judahkirkwood <judahkirkwood@gmail.com>
2024-01-29 23:45:51 -0800

    MathOperations

---
+++ CallingMethods/CallingMethods/MathOperations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class MathOperations
+    {
+        // Adds ten to the number
+        public int Add(int number)
+        {
+            return number + 10;
+        }
+        // Subtracts ten from the number
+        public int Subtract(int number)
+        {
+            return number - 10;
+        }
+        // Multiplies the number by ten
+        public int Multiply(int number) 
+        {
+            return number * 10;
+        }
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Ask the user to input a number
+            Console.WriteLine("Please enter a number: ");
+            // Reads input as a string
+            string userInput = Console.ReadLine();
+
+            // Converts string to an integer
+            if (int.TryParse(userInput, out int userNumber)) 
+            {
+                // Instance of MathOperations created
+                MathOperations mathOps = new MathOperations();
+
+                // Call the Add method
+                Console.WriteLine($"Result of adding: {mathOps.Add(userNumber)}");
+
+                // Call the Subtract method
+                Console.WriteLine($"Result of subtracing: {mathOps.Subtract(userNumber)}");
+
+                // Call the Multiply method 
+                Console.WriteLine($"Result of multiplying: {mathOps.Multiply(userNumber)}");
+            }
+            else 
+            {
+                // Error displayed if not a proper input
+                Console.WriteLine("Invalid number. Please enter a valid number in integer form: ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
