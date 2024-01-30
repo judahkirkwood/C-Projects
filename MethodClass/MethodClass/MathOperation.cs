bd8e6fc9ffcbc380298ed33601fa738497c68969
judahkirkwood <judahkirkwood@gmail.com>
2024-01-30 01:11:52 -0800

    Method Class

---
+++ MethodClass/MethodClass/MathOperation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    public class MathOperation
+    {
+        // Void method that takes two integers, adds 5 to the first integer, and displays the second integer
+        public void Operation1(int number1, int number2)
+        {
+            // Add 5 to the first number
+            int result = number1 + 5;
+
+            // Displaying the second integer to the screen
+            Console.WriteLine($"Result: {result}. Number: {number2}");
+        }
+    }
+}
---
+++ MethodClass/MethodClass/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the MathOperations class
+            MathOperation mathOps = new MathOperation();
+
+            // Call the method, and pass two numbers
+            Console.WriteLine("Result 1");
+            mathOps.Operation1(10, 20);
+
+            // Call the method, specifying the parameters by name
+            Console.WriteLine("\nResult 2 adding 5");
+            mathOps.Operation1(number1: 7, number2: 15);
+
+            // Keep the console window open until the user presses Enter
+            Console.ReadLine();
+        }
+    }
+}
