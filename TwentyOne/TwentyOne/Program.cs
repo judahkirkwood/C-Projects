232de71a96695e25839b2b52ef31bac7ab745430
judahkirkwood <judahkirkwood@gmail.com>
2024-02-14 01:18:15 -0800

    TryCatch

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -16,20 +16,29 @@ namespace TwentyOne
             const string casinoName = "Kirkwood Hotel and Casino";
 
             Console.WriteLine("Welcome to the {0}. Let's start by telling me your name.", casinoName);
+            string playerName = Console.ReadLine();
+
 
             //DateTime yearOfBirth = new DateTime(1985, 4, 11, 8, 32, 6);
             //DateTime yearOfGraduation = new DateTime(2024, 4, 10, 16, 25, 6);
 
             //TimeSpan ageAtGraduation = yearOfGraduation - yearOfBirth;
-            
+
             //Console.WriteLine(ageAtGraduation);
 
-            string text = "Here is some text for the text file.";
-            File.WriteAllText(@"C:\\Users\\Judah Kirkwood\\OneDrive\\Desktop\\Logs.txt", text);
+            //string text = "Here is some text for the text file.";
+            //File.WriteAllText(@"C:\\Users\\Judah Kirkwood\\OneDrive\\Desktop\\Logs.txt", text);
 
-            string playerName = Console.ReadLine();
-            Console.WriteLine("How much money did you bring today?");
-            int bank = Convert.ToInt32(Console.ReadLine());
+            bool validAnswer = false;
+            int bank = 0;
+            while (!validAnswer)
+            {
+                Console.WriteLine("And how much money did you bring today?");
+                validAnswer = int.TryParse(Console.ReadLine(), out bank);
+                if (!validAnswer) Console.WriteLine("Please enter whole number digits only.");
+            }
+
+
             Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now?", playerName);
             string answer = Console.ReadLine().ToLower();
             if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya")
@@ -45,13 +54,28 @@ namespace TwentyOne
                 player.isActivelyPlaying = true;
                 while (player.isActivelyPlaying && player.Balance > 0)
                 {
-                    game.Play();
+                    try
+                    {
+                        game.Play();
+                    }
+                    catch (FraudException)
+                    {
+                        Console.WriteLine("Security!! Kick this person out.");
+                        Console.ReadLine();
+                        return;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("An error occured, please contact your system administrator.");
+                        Console.ReadLine();
+                        return;
+                    }
                 }
                 game -= player;
                 Console.WriteLine("Thank you for playing!");
             }
             Console.WriteLine("Feel free to look around the casino. Bye for now.");
-            Console.ReadLine();
+            Console.Read();
         }
     }
 }
