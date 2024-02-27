e162703a13d8c9646dbe5304dd34c70fed842fa5
judahkirkwood <judahkirkwood@gmail.com>
2024-02-27 12:39:00 -0800

    Newsletter update

--- NewsletterAppMVC/NewsletterAppMVC/Controllers/NewsletterEntities.cs
+++ NewsletterAppMVC/NewsletterAppMVC/Controllers/NewsletterEntities.cs
@@ -1,6 +1,14 @@
-﻿namespace NewsletterAppMVC.Controllers
+﻿using System;
+
+namespace NewsletterAppMVC.Controllers
 {
     internal class NewsletterEntities
     {
+        public object SignUps { get; internal set; }
+
+        internal void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
