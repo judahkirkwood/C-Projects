29559aed2cf9274623b4baf855d506a2ddc52c00
judahkirkwood <judahkirkwood@gmail.com>
2024-02-26 18:26:10 -0800

    NewsletterApp Update

---
+++ NewsletterAppMVC/NewsletterAppMVC/Controllers/dbo.SignUps.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[SignUps] (
+    [Id]           INT           NOT NULL,
+    [FirstName]    VARCHAR (50)  NULL,
+    [LastName]     VARCHAR (50)  NULL,
+    [EmailAddress] VARCHAR (100) NULL,
+    [SocialSecutrityNumber] VARCHAR(15) NULL, 
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
