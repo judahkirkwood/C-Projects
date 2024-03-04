c6fe5db057a7bb18f4a63a141dc9cf25369612a1
JamRaid36 <judahkirkwood@gmail.com>
2024-03-04 14:12:43 -0800

    FinalWebApp

---
+++ FinalWebApp/FinalWebApp/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using FinalWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FinalWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.ServerTime = DateTime.Now;
+            return View();
+        }
+    }
+}
